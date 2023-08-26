7fc3071461fe1397d4a06568994749b496c41d3b
jludena7 <jludena7@hotmail.com>
2023-08-26 11:17:43 -0500

    Enable cors and increase payload size

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { json } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(json({ limit: '68mb' }));
+
   await app.listen(3000);
 }
 bootstrap();
